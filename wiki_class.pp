1881b4bbb6ab9aba9c9546d754a575c8478864f4
vivien224 <87007036+vivien224@users.noreply.github.com>
2021-07-09 09:57:22 +0200

    Update wiki_class.pp

--- wiki_class.pp
+++ wiki_class.pp
@@ -1,3 +1,7 @@
+$src_dir = '/usr/src'
+$dokuwiki_archive = "${src_dir}/dokuwiki.tgz"
+$dokuwiki_dir = "${src_dir}/dokuwiki-2020-07-29"
+
 class development {
   package { 
     'vim':
@@ -18,15 +22,59 @@ class hosting {
   }
 }
 
+class dl_unzip_conf {
+  file {
+  '/usr/src/dokuwiki.tgz':
+    ensure => 'present',
+    source => 'https://download.dokuwiki.org/src/dokuwiki/dokuwiki-stable.tgz',
+    
+    '/var/www/${site_hostname}':
+      ensure  => 'directory',
+      owner   => 'www-data',
+      group   => 'www-data',
+      # mode    => '0755',
+      source  => "${dokuwiki_dir}",
+      recurse => true;
+      
+    '/etc/apache2/sites-available/${site_hostname}.conf':
+      ensure  => present,
+      content => template('/home/vagrant/demo/demo3/site.conf'),
+      require => [Package['apache2'],File['/var/www/${site_hostname}']];
+  }
+  
+  exec {
+  'dokuwiki::unarchive':
+    cwd     => "${src_dir}",
+    command => "tar xavf ${dokuwiki_archive}",
+    creates => "${dokuwiki_dir}",
+    path    => ['/bin','/usr/bin'],
+    require => File["${dokuwiki_archive}"],
+   
+  'enable-vhost-1':
+    command => 'a2ensite ${site_hostname}',
+    path    => ['/usr/bin', '/usr/sbin'],
+    require => [File['/etc/apache2/sites-available/${site_hostname}.conf'],
+      Package['apache2']];
+  }
+
 
 node 'control' { 
   include development
 }
 
 node 'server0' {
+  $site_hostname = 'politique.wiki'
+  $site_dir = 'politique-wiki'
+
   include hosting
+  include dl_unzip_conf
+  
 }
 
 node 'server1' {
+  $site_hostname = 'recette.wiki'
+  $site_dir = 'recette-wiki'
+
   include hosting
+  include dl_unzip_conf
 }
